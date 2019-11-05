71633fd2155e7399b330fadc94024031c277fb1c
MichaelSu37 <ysu3@ualberta.ca>
2019-11-04 22:25:07 -0700

    initial commit

--- w2v.py
+++ w2v.py
@@ -1,6 +1,6 @@
 from gensim.models import Word2Vec
 from gensim.test.utils import common_texts, get_tmpfile
-import re, os
+import re, os, json, nltk
 import numpy as np
 
 def splitFile():
@@ -12,10 +12,63 @@ def splitFile():
             os.mkdir(path)
     
     path = paths[0]
-    files = os.listdir(path)
+    outp = outpath[0]
+    files = sorted(os.listdir(path))
     textfile = []
     truthfile = []
+    for f in files:
+        if (f.endswith(".truth")):
+            truthfile.append(f)
+        else:
+            textfile.append(f)
     
+    fid = 0
+    for j in range(len(textfile)):
+        textname = textfile[j]
+        truthname = truthfile[j]
+        textf = open(textname, 'r')
+        truthf = open(truthname, 'r')
+
+        # get if style is changed, and positions of changing 
+        info = json.load(truthf)
+
+        content = textf.readlines()
+        c = ''
+        for line in content:
+            if (line != '\n'):
+                c += ' ' + line.strip()
+        
+        textf.close()
+        truthf.close()
+
+        # only split the files, and create labeling with 0
+        if (not info["changes"]):
+            lastSentence = 0
+            count = 0
+            sentences = c.split('.')
+            words = c.split()
+
+            segment = ''
+            while (count < len(words) - 200):
+                for k in range(count, count + 200):
+                    if ('.' in words[count + k]):
+                        lastSentence = words[count + k]
+                    segment += words[count + k].replace('.', '')
+
+                textoutname = outp + 'file' + str(fid) + '.txt'
+                truthoutname = outp + 'file' + str(fid) + '.truth'
+                textout = open(textoutname, 'w')
+                truthout = open(truthoutname, 'w')
+
+                textout.write(segment + '\n')
+                truthout.write('0\n')
+
+                fid += 1
+                count += (k + 1)
+
+
+
+
 
 
 if __name__ == '__main__':
