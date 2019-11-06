136d3c8c8f1be3c89962fed481a5a23f78b2eedf
MichaelSu37 <ysu3@ualberta.ca>
2019-11-06 14:51:53 -0700

    need to fix space representation in re

--- w2v.py
+++ w2v.py
@@ -3,13 +3,69 @@ from gensim.test.utils import common_texts, get_tmpfile
 import re, os, json, nltk
 import numpy as np
 
+wnl = nltk.WordNetLemmatizer()
+unwanted = re.compile(r'[^a-z0-9-_\'\s]')
+
+def segFile(fid, info, outp, words, c, styleChanged):
+    if (styleChanged):
+        positions = info["positions"]
+        # partition the file based on style of change, each partition is written by one author
+        partitions = []
+        start = 0
+        for position in positions:
+            pos = sum(partitions) + len(c[start:position + 1])
+            partitions.append(pos)
+            start = position + 1
+        pos = sum(partitions) + len(c[start:].split())
+        partitions.append(pos)
+        partitions = [x - 1 for x in partitions]
+
+    lastSentence = 0
+
+    done = False
+    while not done:
+        ylabel = 0
+        segment = ''
+        try:
+            for k in range(lastSentence, lastSentence + 100):
+                # a sequence of words has cross the point of style change.
+                # meaning style has changed
+                if (styleChanged and k in partitions):
+                    ylabel = 1
+                if ('.' in words[k]):
+                    lastSentence = k
+                segment += words[k].replace('.', '')
+        except IndexError:
+            for k in range(1, 101):
+                segment += words[len(words) - k]
+            done = True
+
+        textoutname = outp + 'file' + str(fid) + '.txt'
+        truthoutname = outp + 'file' + str(fid) + '.truth'
+        textout = open(textoutname, 'w')
+        truthout = open(truthoutname, 'w')
+
+        # write the 100 words segment and the corresponding label to file
+        textout.write(segment + '\n')
+        truthout.write(str(ylabel) + '\n')
+
+        textout.close()
+        truthout.close()
+
+        return fid
+
+
 def splitFile():
+    global wnl
+
     paths = ['testing/', 'training/', 'validation/']
     outpath = ['test/', 'train/', 'validate/']
 
     for path in outpath:
-        if not os.path(path):
+        if not os.path.exists(path):
             os.mkdir(path)
+
+    allWords = []
     
     path = paths[0]
     outp = outpath[0]
@@ -18,9 +74,9 @@ def splitFile():
     truthfile = []
     for f in files:
         if (f.endswith(".truth")):
-            truthfile.append(f)
+            truthfile.append(path + f)
         else:
-            textfile.append(f)
+            textfile.append(path + f)
     
     fid = 0
     for j in range(len(textfile)):
@@ -29,58 +85,56 @@ def splitFile():
         textf = open(textname, 'r')
         truthf = open(truthname, 'r')
 
-        # get if style is changed, and positions of changing 
+        # check if style is changed, and positions of changing 
         info = json.load(truthf)
 
         content = textf.readlines()
         c = ''
         for line in content:
             if (line != '\n'):
-                c += ' ' + line.strip()
+                c += ' ' + line.strip().lower()
         
+        c = unwanted.sub(' ', c)
         textf.close()
         truthf.close()
 
-        # only split the files, and create labeling with 0
+        words = c.split()
+        # TODO: remove plurals using WordNet
+        for w in range(len(words)):
+            #print (words[w])
+            token = wnl.lemmatize(words[w])
+            words[w] = token.encode('utf-8')
+
+        allWords.extend(words)
+
+        # only split the files, and create label with 0
         if (not info["changes"]):
-            lastSentence = 0
-            count = 0
-            sentences = c.split('.')
-            words = c.split()
+            segFile(fid, info, outp, words, c, 0)
+        # need to split the file as well as creating new labels for them
+        else:
+            segFile(fid, info, outp, words, c, 1)
 
-            segment = ''
-            while (count < len(words) - 200):
-                for k in range(count, count + 200):
-                    if ('.' in words[count + k]):
-                        lastSentence = words[count + k]
-                    segment += words[count + k].replace('.', '')
+        fid += 1
 
-                textoutname = outp + 'file' + str(fid) + '.txt'
-                truthoutname = outp + 'file' + str(fid) + '.truth'
-                textout = open(textoutname, 'w')
-                truthout = open(truthoutname, 'w')
-
-                textout.write(segment + '\n')
-                truthout.write('0\n')
-
-                fid += 1
-                count += (k + 1)
-
+    return allWords
 
 
 
 
 
 if __name__ == '__main__':
-    all_words = []
+    d = []
 
     with open('dictionary.txt') as f:
         line = f.readline().strip().lower()
         while line != '':
-            all_words.append(line)
+            d.append(line)
             line = f.readline().strip().lower()
 
-    model = Word2Vec([all_words], min_count = 1, workers = 4)
+    words = splitFile()
+    d.extend(words)
+    wordList = list(set(d))
+    model = Word2Vec([wordList], min_count = 1, workers = 4)
     model.save("word2vec.model")
 
     model = Word2Vec.load("word2vec.model")
