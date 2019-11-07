f0eed55d1b1257d16d3d35b9af16712b1a08b730
MichaelSu37 <ysu3@ualberta.ca>
2019-11-07 12:34:41 -0700

    done splitting files

--- w2v.py
+++ w2v.py
@@ -4,24 +4,25 @@ import re, os, json, nltk
 import numpy as np
 
 wnl = nltk.WordNetLemmatizer()
-unwanted = re.compile(r'[^a-z0-9-_\'\s]')
+unwanted = re.compile('[^a-z0-9-_().!?\']')
+symbols = re.compile('[.!?<>|+]')
 
-def segFile(fid, info, outp, words, c, styleChanged):
+def segFile(fid, fnumber, info, outp, words, c, styleChanged):
     if (styleChanged):
         positions = info["positions"]
         # partition the file based on style of change, each partition is written by one author
-        partitions = []
+        partitions = [0]
         start = 0
         for position in positions:
-            pos = sum(partitions) + len(c[start:position + 1])
+            pos = partitions[-1] + len(c[start:position + 1].split())
             partitions.append(pos)
             start = position + 1
-        pos = sum(partitions) + len(c[start:].split())
+        pos = partitions[-1] + len(c[start:].split())
         partitions.append(pos)
+        partitions.pop(0)
         partitions = [x - 1 for x in partitions]
 
     lastSentence = 0
-
     done = False
     while not done:
         ylabel = 0
@@ -32,16 +33,16 @@ def segFile(fid, info, outp, words, c, styleChanged):
                 # meaning style has changed
                 if (styleChanged and k in partitions):
                     ylabel = 1
-                if ('.' in words[k]):
-                    lastSentence = k
-                segment += words[k].replace('.', '')
+                #if ('.' in words[k]):
+                segment += (' ' + symbols.sub('', words[k]))
+            lastSentence = k
         except IndexError:
             for k in range(1, 101):
                 segment += words[len(words) - k]
             done = True
 
-        textoutname = outp + 'file' + str(fid) + '.txt'
-        truthoutname = outp + 'file' + str(fid) + '.truth'
+        textoutname = outp + 'problem_' + str(fnumber) + '_' + str(fid) + '.txt'
+        truthoutname = outp + 'problem_' + str(fnumber) + '_' + str(fid) + '.truth'
         textout = open(textoutname, 'w')
         truthout = open(truthoutname, 'w')
 
@@ -51,8 +52,8 @@ def segFile(fid, info, outp, words, c, styleChanged):
 
         textout.close()
         truthout.close()
-
-        return fid
+        fid += 1
+    return fid
 
 
 def splitFile():
@@ -67,62 +68,59 @@ def splitFile():
 
     allWords = []
     
-    path = paths[0]
-    outp = outpath[0]
-    files = sorted(os.listdir(path))
-    textfile = []
-    truthfile = []
-    for f in files:
-        if (f.endswith(".truth")):
-            truthfile.append(path + f)
-        else:
-            textfile.append(path + f)
-    
-    fid = 0
-    for j in range(len(textfile)):
-        textname = textfile[j]
-        truthname = truthfile[j]
-        textf = open(textname, 'r')
-        truthf = open(truthname, 'r')
-
-        # check if style is changed, and positions of changing 
-        info = json.load(truthf)
-
-        content = textf.readlines()
-        c = ''
-        for line in content:
-            if (line != '\n'):
-                c += ' ' + line.strip().lower()
+    for i in range(len(paths)):
+        path = paths[i]
+        outp = outpath[i]
+        files = sorted(os.listdir(path))
+        textfile = []
+        truthfile = []
+        for f in files:
+            if (f.endswith(".truth")):
+                truthfile.append(path + f)
+            else:
+                textfile.append(path + f)
         
-        c = unwanted.sub(' ', c)
-        textf.close()
-        truthf.close()
+        fid = 0
+        fnumber = 1
+        for j in range(len(textfile)):
+            textname = textfile[j]
+            truthname = truthfile[j]
+            textf = open(textname, 'r')
+            truthf = open(truthname, 'r')
 
-        words = c.split()
-        # TODO: remove plurals using WordNet
-        for w in range(len(words)):
-            #print (words[w])
-            token = wnl.lemmatize(words[w])
-            words[w] = token.encode('utf-8')
+            # check if style is changed, and positions of changing 
+            info = json.load(truthf)
 
-        allWords.extend(words)
+            content = textf.readlines()
+            c = ''
+            for line in content:
+                if (line != '\n'):
+                    c += ' ' + line.strip().lower()
+            c = unwanted.sub(' ', c)
+            textf.close()
+            truthf.close()
 
-        # only split the files, and create label with 0
-        if (not info["changes"]):
-            segFile(fid, info, outp, words, c, 0)
-        # need to split the file as well as creating new labels for them
-        else:
-            segFile(fid, info, outp, words, c, 1)
+            words = c.split()
+            # TODO: remove plurals using WordNet
+            for w in range(len(words)):
+                #print (words[w])
+                token = wnl.lemmatize(words[w])
+                words[w] = token.encode('utf-8')
 
-        fid += 1
+            allWords.extend(words)
+
+            # only split the files, and create label with 0
+            if (not info["changes"]):
+                fid = segFile(fid, fnumber, info, outp, words, c, 0)
+            # need to split the file as well as creating new labels for them
+            else:
+                fid = segFile(fid, fnumber, info, outp, words, c, 1)
+
+            fnumber += 1
 
     return allWords
 
-
-
-
-
-if __name__ == '__main__':
+def genModel():
     d = []
 
     with open('dictionary.txt') as f:
@@ -139,5 +137,27 @@ if __name__ == '__main__':
 
     model = Word2Vec.load("word2vec.model")
     v = model.wv['computer']
-    print (v)
+    print len(v)
+
+def processData(option):
+    model = Word2Vec.load("word2vec.model")
+    paths = {'test':'testing/', 'train':'training/', 'validate':'validation/'}
+    path = paths[option]
+    files = sorted(os.listdir(path))
+    truthfile = []
+    textfile = []
+
+    for f in files:
+        if (f.endswith(".truth")):
+            truthfile.append(path + f)
+        else:
+            textfile.append(path + f)
+
+
+
+
+if __name__ == '__main__':
+    #genModel()
+    options = ['train', 'test', 'validate']
+    processData(options[0])
 
